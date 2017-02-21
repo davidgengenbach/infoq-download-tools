75b23a3b5127af6498f1cd32024794a57be99600
David Gengenbach <davidgengenbach@gmx.de>
2017-02-21 02:33:17 +0100

    :)

--- create-video.sh
+++ create-video.sh
@@ -2,11 +2,15 @@
 
 set -x
 
-VIDEO_URL_IN='https://www.infoq.com/presentations/Simple-Made-Easy'
+VIDEO_URL_IN="$1"
 FFCONCAT_FILE_IN='ffconcat.txt'
 AUDIO_IN='infoq-11-sep-simplemadeeasy.mp3'
 VIDEO_OUT='infoq-11-sep-simplemadeeasy.mp4'
 FPS=5
+
+if [ $VIDEO_URL_IN == "" ]; then
+    VIDEO_URL_IN='https://www.infoq.com/presentations/Simple-Made-Easy'
+fi
 
 mkdir -p data/slides
 
